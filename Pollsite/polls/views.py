d6bc9d655dc43784667e76fba68bf2f68b0a68d8
nigel <nigelchimwene@gmail.com>
2023-07-07 13:12:33 +0300

    views/template

--- Pollsite/polls/views.py
+++ Pollsite/polls/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import *
 import requests
 
 # Create your views here.
 
 def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
     return HttpResponse("at the poll site")
 
 def detail(request,question_id):
