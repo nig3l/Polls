b9c510088d9b9fa29fde2642004996d387c77858
nigel <nigelchimwene@gmail.com>
2023-07-07 13:56:49 +0300

    template

--- Pollsite/polls/views.py
+++ Pollsite/polls/views.py
@@ -16,6 +16,7 @@ def index(request):
 def detail(request,question_id):
     try:
         question = Question.objects.get(pk=question_id)
+        
     except Question.DoesNotExist:
         raise Http404("Question does not exist") 
     
