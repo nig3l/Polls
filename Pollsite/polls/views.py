5644d179d7dfceba48b64781ad36ed8dd0cb17ea
nigel <nigelchimwene@gmail.com>
2023-07-11 12:52:41 +0300

    views

--- Pollsite/polls/views.py
+++ Pollsite/polls/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
 from django.http import Http404
 from . models import *
+from django.urls import reverse
+
 import requests
 
 # Create your views here.
@@ -29,4 +31,22 @@ def results(request,question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
+
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
     return HttpResponse("You're voting on question %s." % question_id)
